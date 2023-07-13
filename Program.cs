d6405085c17614e77675b46eb386a32f1958cd3c
sunder2568223 <sunderyssr@gmail.com>
2023-07-13 09:40:59 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace excersize1
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter marks in Pre: ");
+            int preMarks = Convert.ToInt32(Console.ReadLine());
+
+            if (preMarks > 60)
+            {
+                Console.Write("Enter marks in Final: ");
+                int finalMarks = Convert.ToInt32(Console.ReadLine());
+
+                if (finalMarks > 55)
+                {
+                    Console.WriteLine("Selected");
+                }
+                else
+                {
+                    Console.WriteLine("Fail in Final");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Fail in Pre");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
